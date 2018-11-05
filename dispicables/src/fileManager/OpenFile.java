2c895dbfddd991bdc04a83046a24ee7401fc31a7
Kelvin <kelvin.costa@cognizant.com>
2018-11-04 23:45:28 -0200

    Criadas classes que realização gerenciamento de abertura e escrita em arquivos de textos

---
+++ dispicables/src/fileManager/OpenFile.java
@@ -0,0 +1,12 @@
+package fileManager;
+
+public class OpenFile {
+
+	/*
+	 * 
+	 * Classe deve verificar se arquivo existe
+	 * Se sim, retorna arquivo existente
+	 * Se não, cria arquivo e retorna arquivo recem criado
+	 * 
+	 */
+}
---
+++ dispicables/src/fileManager/WriteFile.java
@@ -0,0 +1,6 @@
+package fileManager;
+
+public class WriteFile {
+
+	//TODO: Criar forma de escrita para arquivo
+}
