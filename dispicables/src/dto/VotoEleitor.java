c309dc70496f6a43cec27d24307cf08c22baf43b
Kelvin Costa <kelvin.costa@fatec.sp.gov.br>
2018-11-04 23:20:27 -0200

    Iniciando projeto; Adicionando classes dto que armazenarão as informações vindas do banco para que sejam utilizadas pelo sistema

---
+++ dispicables/src/dto/VotoEleitor.java
@@ -0,0 +1,23 @@
+package dto;
+
+public class VotoEleitor {
+
+	Proposicao proposicao;
+	Eleitor eleitor;
+	
+	public Proposicao getProposicao() {
+		return proposicao;
+	}
+	public void setProposicao(Proposicao proposicao) {
+		this.proposicao = proposicao;
+	}
+	public Eleitor getEleitor() {
+		return eleitor;
+	}
+	public void setEleitor(Eleitor eleitor) {
+		this.eleitor = eleitor;
+	}
+	
+	
+	
+}
