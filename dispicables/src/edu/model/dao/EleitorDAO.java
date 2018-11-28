097ef1974ed3b797522abe8810a8aa9b484419cf
Kelvin Vicente Costa <kelvin.costa@fatec.sp.gov.br>
2018-11-28 17:44:42 -0200

    Comparativo
    Adicionando metodo que realiza comparativo dos votos do eleitor com os votos do parlamentar

--- dispicables/src/edu/model/dao/EleitorDAO.java
+++ dispicables/src/edu/model/dao/EleitorDAO.java
@@ -26,4 +26,69 @@ public class EleitorDAO extends DAO {
         return selectBoolean(query);
     }
 
+    private int getEleitorId(String email) throws SQLException, ClassNotFoundException {
+        String query = "SELECT id FROM eleitor WHERE email = '" + email + "'";
+        ResultSet rs = selectRS(query);
+        return rs.getInt("id");
+    }
+
+    /**
+     * Classe para comparação de votos do eleitor com os parlamentares
+     * Retorna os seguintes headers dentro do ResultSet:
+     *  deputado_id
+     *  nome_deputador
+     *  votos_iguais
+     *  votos_totais
+     * @param eleitor
+     * @return
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     *
+     */
+    public ResultSet getComparacao(Eleitor eleitor) throws SQLException, ClassNotFoundException {
+        int id = getEleitorId(eleitor.getEmail());
+        String query = "select\n" +
+                "\tq_vd.deputado_id,\n" +
+                "\td.nome nome_deputado,\n" +
+                "\tsum(case when ve.voto = q_vd.voto then 1 else 0 end) votos_iguais, \n" +
+                "\tcount(case when ve.voto is not null and q_vd.voto is not null then 1 else 0 end) votos_totais\n" +
+                "from voto_eleitor ve\n" +
+                "inner join (\n" +
+                "\tselect \n" +
+                "\t\tq1.proposicao_id,\n" +
+                "\t\tq1.deputado_id,\n" +
+                "\t\tq1.voto\n" +
+                "\tfrom (\n" +
+                "\t\tselect  \n" +
+                "\t\t\tv.proposicao_id, \n" +
+                "\t\t\tvd.deputado_id, \n" +
+                "\t\t\tvd.voto,\n" +
+                "\t\t\tv.data_votacao\n" +
+                "\t\tfrom votacao v\n" +
+                "\t\tinner join voto_deputado vd\n" +
+                "\t\ton v.id = vd.votacao_id\n" +
+                "\t) q1\n" +
+                "\tinner join (\n" +
+                "\t\tselect  \n" +
+                "\t\t\tv.proposicao_id, \n" +
+                "\t\t\tvd.deputado_id, \n" +
+                "\t\t\tmax(v.data_votacao) as data_votacao\n" +
+                "\t\tfrom votacao v\n" +
+                "\t\tinner join voto_deputado vd\n" +
+                "\t\ton v.id = vd.votacao_id\n" +
+                "\t\tgroup by v.proposicao_id, vd.deputado_id\n" +
+                "\t) q2\n" +
+                "\ton \n" +
+                "\t\tq1.proposicao_id = q2.proposicao_id \n" +
+                "\t\tand q1.deputado_id = q2.deputado_id\n" +
+                "\t\tand q1.data_votacao = q2.data_votacao\n" +
+                ") q_vd\n" +
+                "on ve.proposicao_id = q_vd.proposicao_id\n" +
+                "inner join deputado d\n" +
+                "on q_vd.deputado_id = d.id \n" +
+                "where ve.eleitor_id = " + id + "\n" +
+                "group by q_vd.deputado_id, d.nome;";
+
+        return selectRS(query);
+    }
 }
