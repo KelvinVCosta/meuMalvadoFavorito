4cdacb4a1777570e53dfd685861a4ad853d9eb82
João Henrique <jhsmaciel15@gmail.com>
2018-11-30 01:57:57 -0200

    Apostofro na data

--- insertExample.sql
+++ insertExample.sql
@@ -10,13 +10,13 @@ insert into tipo_proposicao (id,sigla,nome,descricao) values (8,'HHH','NOME TIPO
 insert into tipo_proposicao (id,sigla,nome,descricao) values (9,'III','NOME TIPOM PROPOSICAO 9','DESCRITION TIPO PROPOSICAO 9');
 insert into tipo_proposicao (id,sigla,nome,descricao) values (10,'JJJ','NOME TIPOM PROPOSICAO 10','DESCRITION TIPO PROPOSICAO 10');
 -- INSERT NA PROPOSICAO
-insert into proposicao (id,ementa,data_apresentacao,ementa_detalhada, texto, tipo_proposicao_id) values (1,'EMENTA EXEMPlO 1', 2016-06-11, 'EMENTA DETAlHE EXAMPlE 1', 'TEXT EMENTA 1',1);
-insert into proposicao (id,ementa,data_apresentacao,ementa_detalhada, texto, tipo_proposicao_id) values (2,'EMENTA EXEMPlO 2', 2016-06-11, 'EMENTA DETAlHE EXAMPlE 2', 'TEXT EMENTA 2',2);
-insert into proposicao (id,ementa,data_apresentacao,ementa_detalhada, texto, tipo_proposicao_id) values (3,'EMENTA EXEMPlO 3', 2016-06-11, 'EMENTA DETAlHE EXAMPlE 3', 'TEXT EMENTA 3',3);
-insert into proposicao (id,ementa,data_apresentacao,ementa_detalhada, texto, tipo_proposicao_id) values (4,'EMENTA EXEMPlO 4', 2016-06-11, 'EMENTA DETAlHE EXAMPlE 4', 'TEXT EMENTA 4',4);
-insert into proposicao (id,ementa,data_apresentacao,ementa_detalhada, texto, tipo_proposicao_id) values (5,'EMENTA EXEMPlO 5', 2016-06-11, 'EMENTA DETAlHE EXAMPlE 5', 'TEXT EMENTA 5',5);
-insert into proposicao (id,ementa,data_apresentacao,ementa_detalhada, texto, tipo_proposicao_id) values (6,'EMENTA EXEMPlO 6', 2016-06-11, 'EMENTA DETAlHE EXAMPlE 6', 'TEXT EMENTA 6',6);
-insert into proposicao (id,ementa,data_apresentacao,ementa_detalhada, texto, tipo_proposicao_id) values (7,'EMENTA EXEMPlO 7', 2016-06-11, 'EMENTA DETAlHE EXAMPlE 7', 'TEXT EMENTA 7',7);
-insert into proposicao (id,ementa,data_apresentacao,ementa_detalhada, texto, tipo_proposicao_id) values (8,'EMENTA EXEMPlO 8', 2016-06-11, 'EMENTA DETAlHE EXAMPlE 8', 'TEXT EMENTA 8',8);
-insert into proposicao (id,ementa,data_apresentacao,ementa_detalhada, texto, tipo_proposicao_id) values (9,'EMENTA EXEMPlO 9', 2016-06-11, 'EMENTA DETAlHE EXAMPlE 9', 'TEXT EMENTA 9',9);
-insert into proposicao (id,ementa,data_apresentacao,ementa_detalhada, texto, tipo_proposicao_id) values (10,'EMENTA EXEMPlO 10',2016-06-11,'EMENTA DETAlHE EXAMPlE 10','TEXT EMENTA 10',10);
+insert into proposicao (id,ementa,data_apresentacao,ementa_detalhada, texto, tipo_proposicao_id) values (1,'EMENTA EXEMPlO 1', '2016-06-11', 'EMENTA DETAlHE EXAMPlE 1', 'TEXT EMENTA 1',1);
+insert into proposicao (id,ementa,data_apresentacao,ementa_detalhada, texto, tipo_proposicao_id) values (2,'EMENTA EXEMPlO 2', '2016-06-11', 'EMENTA DETAlHE EXAMPlE 2', 'TEXT EMENTA 2',2);
+insert into proposicao (id,ementa,data_apresentacao,ementa_detalhada, texto, tipo_proposicao_id) values (3,'EMENTA EXEMPlO 3', '2016-06-11', 'EMENTA DETAlHE EXAMPlE 3', 'TEXT EMENTA 3',3);
+insert into proposicao (id,ementa,data_apresentacao,ementa_detalhada, texto, tipo_proposicao_id) values (4,'EMENTA EXEMPlO 4', '2016-06-11', 'EMENTA DETAlHE EXAMPlE 4', 'TEXT EMENTA 4',4);
+insert into proposicao (id,ementa,data_apresentacao,ementa_detalhada, texto, tipo_proposicao_id) values (5,'EMENTA EXEMPlO 5', '2016-06-11', 'EMENTA DETAlHE EXAMPlE 5', 'TEXT EMENTA 5',5);
+insert into proposicao (id,ementa,data_apresentacao,ementa_detalhada, texto, tipo_proposicao_id) values (6,'EMENTA EXEMPlO 6', '2016-06-11', 'EMENTA DETAlHE EXAMPlE 6', 'TEXT EMENTA 6',6);
+insert into proposicao (id,ementa,data_apresentacao,ementa_detalhada, texto, tipo_proposicao_id) values (7,'EMENTA EXEMPlO 7', '2016-06-11', 'EMENTA DETAlHE EXAMPlE 7', 'TEXT EMENTA 7',7);
+insert into proposicao (id,ementa,data_apresentacao,ementa_detalhada, texto, tipo_proposicao_id) values (8,'EMENTA EXEMPlO 8', '2016-06-11', 'EMENTA DETAlHE EXAMPlE 8', 'TEXT EMENTA 8',8);
+insert into proposicao (id,ementa,data_apresentacao,ementa_detalhada, texto, tipo_proposicao_id) values (9,'EMENTA EXEMPlO 9', '2016-06-11', 'EMENTA DETAlHE EXAMPlE 9', 'TEXT EMENTA 9',9);
+insert into proposicao (id,ementa,data_apresentacao,ementa_detalhada, texto, tipo_proposicao_id) values (10,'EMENTA EXEMPlO 10','2016-06-11','EMENTA DETAlHE EXAMPlE 10','TEXT EMENTA 10',10);
